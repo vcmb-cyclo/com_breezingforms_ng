20d4416b1f7d98d105cab659f0fe4218e5622bf1
Xavier DANO <xavier.dano@bbox.fr>
2024-07-03 19:28:04 +0200

    Remove a import('joomla.html.pagination');

--- administrator/components/com_contentbuilder/models/storages.php
+++ administrator/components/com_contentbuilder/models/storages.php
@@ -187,7 +187,6 @@ class ContentbuilderModelStorages extends CBModel
     {
         // Load the content if it doesn't already exist
         if (empty($this->_pagination)) {
-            jimport('joomla.html.pagination');
             $this->_pagination = new Pagination($this->getTotal(), $this->getState('limitstart'), $this->getState('limit'));
         }
         return $this->_pagination;
